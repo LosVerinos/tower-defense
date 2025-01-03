e9221fa19dedcb8b802323719f727117bc43fd6f
Pichon Sebastien <arthox.mail@gmail.com>
2025-01-03 23:39:13 +0100

    jsuis fatigue donc commit (ajout Pakcage + debut UI shop)

---
+++ OniDefense/Assets/Scripts/ShopScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+    public void PurschaseMitrailleuse(){
+        Debug.Log("Mitrailleuse achetée");
+    }
+
+    public void PurschaseSniper(){
+        Debug.Log("Sniper achetée");
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
