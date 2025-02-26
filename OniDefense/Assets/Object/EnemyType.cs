bcdb14355e221c36afeb70a0d30863fefe9f01bf
Robin <robin.bechlem@gmail.com>
2025-02-26 23:53:46 +0100

    👍🏻refactor

--- OniDefense/Assets/Object/EnemyType.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-public enum EnemyType
-{
-    Feu,
-    Glace
-}
---
+++ OniDefense/Assets/object/Defense.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public abstract class Defense
+    {
+        private string name;
+        private int upgradeLevel;
+        private List<DefenseUpgradeState> upgradeState;
+        private Transform target;
+        private LayerMask ennemyLayer;
+        private Transform movingPartx;
+        private Transform movingParty;
+        private bool active;
+
+        public Defense()
+        {
+
+        }
+
+        public void Shoot(Ennemy Target)
+        {
+
+        }
+        public Transform findTarget()
+        {
+            return null;
+        }
+
+    }
+
+}
+
