9c717127dacb0e8d3defc884cc301a716027510b
Pichon Sebastien <arthox.mail@gmail.com>
2025-03-02 17:06:15 +0100

    Changing stats to display

--- OniDefense/Assets/Scripts/StatsUIScript.cs
+++ OniDefense/Assets/Scripts/StatsUIScript.cs
@@ -10,8 +10,10 @@ public class StatsUIScript : MonoBehaviour
     public TextMeshProUGUI statsText;
 
     public void DisplayStats(){
-        statsText.text = $"Vagues survécues: {PlayerStats.PassedWaves} \n" +
-                         $"Argent: {PlayerStats.Money} \n" +
-                         $"Vies: {PlayerStats.Lives.ToString()}";
+        statsText.text = $"Vagues survécues : {PlayerStats.PassedWaves} \n" +
+                         $"Argent dépensé : {PlayerStats.MoneySpent} \n" +
+                         $"Zombies tués : {PlayerStats.NbKilledEmenies} \n" +
+                         $"Dégats infligés : {PlayerStats.DamagesGiven} \n" +
+                         $"Défenses construites : {PlayerStats.BuiltDefenses}";
     }
 }
